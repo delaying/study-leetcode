ef4aa570f942d1a8d29eb69f524d474ed8d0c149
jiyeon-bloomingbit <delay@bloomingbit.io>
2025-04-24 16:24:55 +0900

    4/24

---
+++ src/83-remove-duplicate-from-sorted-list.ts
@@ -0,0 +1,42 @@
+// Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.
+
+// Input: head = [1,1,2]
+// Output: [1,2]
+
+// Input: head = [1,1,2,3,3]
+// Output: [1,2,3]
+
+class ListNode {
+  val: number;
+  next: ListNode | null;
+  constructor(val?: number, next?: ListNode | null) {
+    this.val = val === undefined ? 0 : val;
+    this.next = next === undefined ? null : next;
+  }
+}
+
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  let current = head;
+
+  while (current && current.next) {
+    if (current.val === current.next.val) {
+      current.next = current.next.next;
+    } else {
+      current = current.next;
+    }
+  }
+
+  return head;
+}
+
+console.log(
+  deleteDuplicates(new ListNode(1, new ListNode(1, new ListNode(2))))
+);
+console.log(
+  deleteDuplicates(
+    new ListNode(
+      1,
+      new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(3))))
+    )
+  )
+);
